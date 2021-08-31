f75f2fc55619a19ad8fef1d28a1e2cbb2ecd9d8c
Durss <francois.dursus@gmail.com>
2021-09-01 00:04:08 +0200

    Way more stressproof  binary parsing Previously it was failing simultaeous key press/release detection.

--- src_back/controllers/HIDController.ts
+++ src_back/controllers/HIDController.ts
@@ -1,5 +1,5 @@
+import { Express } from "express-serve-static-core";
 import { Client } from 'node-osc';
-import { Express, Request, Response } from "express-serve-static-core";
 import SocketServer, { SOCK_ACTIONS } from "../server/SocketServer";
 import Config from "../utils/Config";
 import Logger from "../utils/Logger";
@@ -11,9 +11,9 @@ export default class HIDController {
 
 	private static _instance:HIDController;
 	private _app:Express;
-	private oscReady:boolean;
 	private oscClient:Client;
 	private id:number = 0;
+	private idToState:{[key:string]:string} = {};
 	
 	constructor() {
 	
@@ -54,27 +54,104 @@ export default class HIDController {
 			var device = new HID.HID( "/dev/input/by-id/usb-0513_0318-event-kbd" );
 			device.on("data", (data:Buffer) => {
 				let str = data.toString('hex');
-				let keyIndex = str.indexOf("000100");
-				let keyCode = str.substr(keyIndex+6,2);
-				let inputType = str.substr(keyIndex+10,2);
-				this.id = ++this.id%1000;
-				// console.log(keyCode, inputType);
-				let action;
-				if(inputType == "01") action = SOCK_ACTIONS.KEY_DOWN;
-				if(inputType == "02") action = SOCK_ACTIONS.KEY_PRESSED;
-				if(inputType == "00") action = SOCK_ACTIONS.KEY_UP;
-				//Send via OSC
-				this.oscClient.send("/key/"+this.id+"/"+action+"/"+keyCode, (error):void => {
-					if(error) {
-						Logger.error("OSC error");
-						console.log(error);
-					}
-				});
-				//Send via WebSocket
-				SocketServer.instance.broadcast({action, data:{keyCode, id:this.id}});
+				// console.log(str);
+				// console.log("\r\n");
+				let chunks = str.match(/.{32}/g);
+				for (let i = 0; i < chunks.length; i++) {
+					let line = chunks[i];
+					if(/000700$/gi.test(line)) continue;//Dunno the meaning of these lines
+					if(/0{18}$/gi.test(line)) continue;//Dunno the meaning of these lines
+					let data = line.substr(14).match(/.{4}/g);
+					if(data[0] != "0001") continue;//Dunno the meaning of these lines
+					let keyCode = data[1].substr(2);
+					let inputType = data[2].substr(2);
+					
+					let action;
+					if(inputType == "01") action = SOCK_ACTIONS.KEY_DOWN;
+					if(inputType == "02") action = SOCK_ACTIONS.KEY_PRESSED;
+					if(inputType == "00") action = SOCK_ACTIONS.KEY_UP;
+
+					this.broadcast(inputType, keyCode);
+				}
 			});
 		} catch(e) {
 			Logger.error(e);
 		}
 	}
+
+	/**
+	 * Broadcast a key event through websocket and OSC
+	 * 
+	 * @param inputType 
+	 * @param keyCode 
+	 * @returns 
+	 */
+	private broadcast(inputType:string, keyCode:string):void {
+		if(this.idToState[keyCode] == inputType) return;
+		this.idToState[keyCode] = inputType;
+
+		this.id++;
+		let action;
+		if(inputType == "01") action = SOCK_ACTIONS.KEY_DOWN;
+		if(inputType == "02") action = SOCK_ACTIONS.KEY_PRESSED;
+		if(inputType == "00") {
+			action = SOCK_ACTIONS.KEY_UP;
+			delete this.idToState[keyCode];
+		}
+		//Send via OSC
+		this.oscClient.send("/key/"+this.id+"/"+action+"/"+keyCode, (error):void => {
+			if(error) {
+				Logger.error("OSC error");
+				console.log(error);
+			}
+		});
+		//Send via WebSocket
+		SocketServer.instance.broadcast({action, data:{keyCode, id:this.id}});
+	}
 }
+
+/********************************************
+ ********** KEYBOARD DATA EXAMPLES **********
+ ********************************************
+
+//PRESS 1 & 2 at the same time
+73882e617ffa01		0004	0004	001e	000700
+73882e617ffa01		0001	0002	0001	000000
+73882e617ffa01		0004	0004	001f	000700
+73882e617ffa01		0001	0003	0001	000000
+73882e617ffa01		0000	0000	0000	000000
+
+
+//PRESS ESCAPE
+e4482e617b5803		0004	0004	0029	000700
+e4482e617b5803		0001	0001	0001	000000
+e4482e617b5803		0000	0000	0000	000000
+
+//PRESS 1
+e4482e61214e06		0004	0004	001e	000700
+e4482e61214e06		0001	0002	0001	000000
+e4482e61214e06		0000	0000	0000	000000
+
+
+
+//KEEP PRESSED ESCAPE
+9a862e6160a409		0001	0001	0002	000000
+9a862e6160a409		0000	0000	0001	000000
+
+//KEEP PRESSED 1
+c3862e61b66601		0001	0002	0002	000000
+c3862e61b66601		0000	0000	0001	000000
+
+
+
+//RELEASE ESCAPE
+e5482e61f1de04		0004	0004	0029	000700
+e5482e61f1de04		0001	0001	00000	00000
+e5482e61f1de04		0000	0000	0000	000000
+
+//RELEASE 1
+e5482e61311907		0004	0004	001e	000700
+e5482e61311907		0001	0002	0000	000000
+e5482e61311907		0000	0000	0000	000000
+
+ */
